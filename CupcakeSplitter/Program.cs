f7b02ee2d7c9134422b85c93ae0ea5ff9c51edbe
James M. Allen <james.m.allen@gmail.com>
2016-04-20 10:22:22 -0400

    Initial version of cupcake splitter

---
+++ CupcakeSplitter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CupcakeSplitter
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("How many cupcakes do you have?");
+            int cupcakes = int.Parse(Console.ReadLine());
+            Console.WriteLine("How many people are sharing?");
+            int people = int.Parse(Console.ReadLine());
+
+            int cupcakesPerPerson = cupcakes / people;
+
+            int remainingCupcakes = cupcakes % people;
+
+            Console.WriteLine("Each person gets {0} cupcakes, and there are {1} left over.",
+                              cupcakesPerPerson, remainingCupcakes);
+
+        }
+    }
+}
