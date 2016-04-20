09aa786ec270bd1fb0cd1721b6251c95361e6916
James M. Allen <james.m.allen@gmail.com>
2016-04-20 10:44:02 -0400

    Handling exceptions

--- CupcakeSplitter/Program.cs
+++ CupcakeSplitter/Program.cs
@@ -6,17 +6,29 @@ namespace CupcakeSplitter
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("How many cupcakes do you have?");
-            int cupcakes = int.Parse(Console.ReadLine());
-            Console.WriteLine("How many people are sharing?");
-            int people = int.Parse(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("How many cupcakes do you have?");
+                int cupcakes = int.Parse(Console.ReadLine());
+                Console.WriteLine("How many people are sharing?");
+                int people = int.Parse(Console.ReadLine());
 
-            int cupcakesPerPerson = cupcakes / people;
+                int cupcakesPerPerson = cupcakes / people;
 
-            int remainingCupcakes = cupcakes % people;
+                int remainingCupcakes = cupcakes % people;
 
-            Console.WriteLine("Each person gets {0} cupcakes, and there are {1} left over.",
-                              cupcakesPerPerson, remainingCupcakes);
+                Console.WriteLine("Each person gets {0} cupcakes, and there are {1} left over.",
+                    cupcakesPerPerson, remainingCupcakes);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("You can't split cupcakes with zero people!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please type a whole number!");
+            }
+
 
         }
     }
