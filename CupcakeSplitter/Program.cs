28f1364ae98edd58684e38d4032c20579328c854
James M. Allen <james.m.allen@gmail.com>
2016-04-20 11:02:02 -0400

    Back to sanity

--- CupcakeSplitter/Program.cs
+++ CupcakeSplitter/Program.cs
@@ -24,10 +24,6 @@ namespace CupcakeSplitter
             {
                 Console.WriteLine("You can't split cupcakes with zero people!");
             }
-            catch (ArithmeticException)
-            {
-                Console.WriteLine("We got an arithmetic exception, what?");
-            }
             catch (FormatException)
             {
                 Console.WriteLine("Please type a whole number!");
